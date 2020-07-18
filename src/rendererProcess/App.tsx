e9e36a73f915ac0fcd96c1906e676ba9f84bb16f
d0kur0 <dev.dokuro@gmail.com>
2020-07-18 13:58:58 +0300

    Add download queue for download buttons

--- src/rendererProcess/App.tsx
+++ src/rendererProcess/App.tsx
@@ -1,7 +1,13 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { handleGalleryFetchRequest } from "./redux/gallery/thunks";
 import { RootState } from "./redux";
+
+type DownloadQueue = {
+  url: string;
+  ended: boolean;
+  error?: string;
+}[];
 
 export default function App() {
   const dispath = useDispatch();
@@ -12,42 +18,68 @@ export default function App() {
   }, [dispath]);
 
   const { errors, photos, loading } = useSelector((state: RootState) => state.gallery);
+  const [downloadQueue, setDownloadQueue] = useState<DownloadQueue>([]);
 
-  const donwloadFile = (url: string) => {
+  const downloadFile = (url: string) => {
     ipcRenderer.send("download-image", { url });
+
+    setDownloadQueue(queue =>
+      queue.some(item => item.url === url)
+        ? queue.map(item => (item.url === url ? { ...item, ended: false } : item))
+        : [...queue, { url, ended: false }],
+    );
   };
 
   ipcRenderer.on("download-response", (event, args) => {
     console.log(args);
+
+    setDownloadQueue(queue =>
+      queue.map(item => {
+        if (item.url === args.url) {
+          item = { ...item, ended: true };
+          args.error && (item.error = args.error);
+        }
+
+        return item;
+      }),
+    );
   });
 
   const Loader = () => (
-    <div className="loading">
-      <svg className="spinner" width="65px" height="65px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
-        <circle className="path" fill="none" strokeWidth="6" strokeLinecap="round" cx="33" cy="33" r="30"></circle>
-      </svg>
-    </div>
+    <svg className="spinner" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
+      <circle className="path" fill="none" strokeWidth="6" strokeLinecap="round" cx="33" cy="33" r="30"></circle>
+    </svg>
   );
   const Error = () => <div className="error">{errors}</div>;
   const Photos = () => (
     <div className="photos">
-      {photos.map((photo, index) => (
-        <div className="photo" key={index}>
-          <span className="photo-id">#{photo.id}</span>
-          <div className="photo-title">{photo.title}</div>
-          <img className="photo-image" src={photo.thumbnailUrl} alt={photo.title} />
-          <button onClick={() => donwloadFile(photo.url)} className="photo-download-button">
-            Download
-          </button>
-        </div>
-      ))}
+      {photos.map((photo, index) => {
+        const isInQueue = downloadQueue.some(item => item.url === photo.url && !item.ended);
+
+        return (
+          <div className="photo" key={index}>
+            <span className="photo-id">#{photo.id}</span>
+            <div className="photo-title">{photo.title}</div>
+            <img className="photo-image" src={photo.thumbnailUrl} alt={photo.title} />
+            <button disabled={isInQueue} onClick={() => downloadFile(photo.url)} className="photo-download-button">
+              {isInQueue ? <Loader /> : `Download`}
+            </button>
+          </div>
+        );
+      })}
     </div>
   );
 
   return (
     <section className="gallery">
       <h2>Photo gallery example</h2>
-      {loading && <Loader />}
+
+      {loading && (
+        <div className="loading">
+          <Loader />
+        </div>
+      )}
+
       {errors ? <Error /> : <Photos />}
     </section>
   );
