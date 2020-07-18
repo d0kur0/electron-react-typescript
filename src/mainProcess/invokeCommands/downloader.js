7911bc706cd99d30b20ec166000db1be157a913e
d0kur0 <dev.dokuro@gmail.com>
2020-07-18 13:16:58 +0300

    blob -> buffer

--- src/mainProcess/invokeCommands/downloader.js
+++ src/mainProcess/invokeCommands/downloader.js
@@ -2,19 +2,18 @@ const { dialog } = require("electron");
 const { BrowserWindow } = require("electron");
 const fs = require("fs");
 const fetch = require("node-fetch");
-const { electron } = require("process");
 
 const downloader = async (event, { url }) => {
   const request = await fetch(url);
 
-  if (request.status !== 200) {
+  if (!request.ok) {
     return event.reply("download-response", { url, error: "Download error" });
   }
 
-  const imageAsBlob = await request.blob();
+  const imageBuffer = await request.buffer();
   const dialogResponse = await dialog.showSaveDialog(BrowserWindow.getFocusedWindow(), {
     filters: [
-      { name: "Изображения", extensions: ["jpg", "png", "gif"] },
+      { name: "PNG Изображения", extensions: ["png"] },
       { name: "Все файлы", extensions: ["*"] },
     ],
   });
@@ -24,7 +23,7 @@ const downloader = async (event, { url }) => {
   }
 
   fs.promises
-    .writeFile(dialogResponse.filePath, imageAsBlob)
+    .writeFile(dialogResponse.filePath, imageBuffer)
     .then(() => event.reply("download-response", { url, success: "Image downloaded" }))
     .catch(() => event.reply("download-response", { url, error: "Save file error" }));
 };
