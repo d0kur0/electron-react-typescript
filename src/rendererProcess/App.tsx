9b42397ef99e24bf2c83660d1dc59f960481ea68
d0kur0 <dev.dokuro@gmail.com>
2020-07-18 12:06:52 +0300

    Добавил вывод фотографий из стора

--- src/rendererProcess/App.tsx
+++ src/rendererProcess/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { handleGalleryFetchRequest } from "./redux/gallery/thunks";
+import { RootState } from "./redux";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function App() {
+  const dispath = useDispatch();
+  const { ipcRenderer } = window.require("electron");
+
+  useEffect(() => {
+    dispath(handleGalleryFetchRequest());
+  }, [dispath]);
+
+  const { errors, photos, loading } = useSelector((state: RootState) => state.gallery);
+
+  const donwloadFile = (url: string) => {
+    ipcRenderer.send("download-image", { url });
+  };
+
+  ipcRenderer.on("download-response", (event, args) => {
+    console.log(args);
+  });
+
+  const Loader = () => (
+    <div className="loading">
+      <svg className="spinner" width="65px" height="65px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
+        <circle className="path" fill="none" strokeWidth="6" strokeLinecap="round" cx="33" cy="33" r="30"></circle>
+      </svg>
     </div>
   );
-}
+  const Error = () => <div className="error">{errors}</div>;
+  const Photos = () => (
+    <div className="photos">
+      {photos.map((photo, index) => (
+        <div className="photo" key={index}>
+          <span className="photo-id">#{photo.id}</span>
+          <div className="photo-title">{photo.title}</div>
+          <img className="photo-image" src={photo.thumbnailUrl} alt={photo.title} />
+          <button onClick={() => donwloadFile(photo.url)} className="photo-download-button">
+            Download
+          </button>
+        </div>
+      ))}
+    </div>
+  );
 
-export default App;
+  return (
+    <section className="gallery">
+      <h2>Photo gallery example</h2>
+      {loading && <Loader />}
+      {errors ? <Error /> : <Photos />}
+    </section>
+  );
+}
